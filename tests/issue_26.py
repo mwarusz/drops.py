a9ca0c2b440cd41931ec67304b81e29dd4f98c80
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2016-10-28 01:32:16 +0200

    change os.system to subprocess

--- tests/issue_26.py
+++ tests/issue_26.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
-import tempfile, os, sys
+import tempfile, subprocess, sys
 
 outdir = tempfile.mkdtemp()
 srcdir = sys.argv[1]
 
-os.system(srcdir + "/drops.py --outdir " + outdir + 
+out = subprocess.check_output(srcdir + "/drops.py --outdir " + outdir + 
   " --T 288 --p 100000 --RH 0.999 --w 0.62593 --outfreq 10000" + 
   " --dt 0.5 --nt 320 lgrngn --sd_conc 128 --kappa 0.6052" + 
   " --n_tot 930759410 317786280 --meanr 1.1429e-07 4.3822e-08" + 
-  " --gstdv 1.4745 1.4466 --cloud_r_min 2e-6"
+  " --gstdv 1.4745 1.4466 --cloud_r_min 2e-6",
+  shell=True
 )
+print out
 
 f = open(outdir + "/stats.gpi")
 for line in f:
