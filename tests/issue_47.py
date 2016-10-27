8c3e2a1b73c6c0421e8a1cb4a24ca56ba81679f5
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2016-10-28 01:55:22 +0200

    merge stderr and stdout

--- tests/issue_47.py
+++ tests/issue_47.py
@@ -8,6 +8,7 @@ srcdir = sys.argv[1]
 out = subprocess.check_output(srcdir + "/drops.py --outdir " + outdir + 
   " --T 300 --p 100000 --RH .99 --w 1 --dt 1 --nt 2000 --outfreq 10"
   " lgrngn --sd_conc 64 --kappa .8 --n_tot 500e6 --meanr .05e-6 --gstdv 2",
-  shell=True
+  shell=True,
+  stderr=subprocess.STDOUT
 )
 print out
