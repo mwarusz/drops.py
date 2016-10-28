2ec056983c6549dacf26321ce2029f261c60e817
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2016-10-28 11:01:12 +0200

    ...

--- tests/issue_26.py
+++ tests/issue_26.py
@@ -5,15 +5,17 @@ import tempfile, subprocess, sys
 outdir = tempfile.mkdtemp()
 srcdir = sys.argv[1]
 
-out = subprocess.check_output(srcdir + "/drops.py --outdir " + outdir + 
-  " --T 288 --p 100000 --RH 0.999 --w 0.62593 --outfreq 10000" + 
-  " --dt 0.5 --nt 320 lgrngn --sd_conc 128 --kappa 0.6052" + 
-  " --n_tot 930759410 317786280 --meanr 1.1429e-07 4.3822e-08" + 
-  " --gstdv 1.4745 1.4466 --cloud_r_min 2e-6",
-  shell=True,
-  stderr=subprocess.STDOUT
-)
-print out
+try:
+    out = subprocess.check_output(srcdir + "/drops.py --outdir " + outdir + 
+      " --T 288 --p 100000 --RH 0.999 --w 0.62593 --outfreq 10000" + 
+      " --dt 0.5 --nt 320 lgrngn --sd_conc 128 --kappa 0.6052" + 
+      " --n_tot 930759410 317786280 --meanr 1.1429e-07 4.3822e-08" + 
+      " --gstdv 1.4745 1.4466 --cloud_r_min 2e-6",
+      shell=True
+    )
+except subprocess.CalledProcessError as e:
+    print e.output
+    print out
 
 f = open(outdir + "/stats.gpi")
 for line in f:
